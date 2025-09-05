f24c458efaa25c709e3e6dd5cc06837f29dcd620
Mohammad23h <mohammmad2.3h@gmail.com>
2025-09-06 00:18:55 +0300

    add certificates to doctors

--- routes/api.php
+++ routes/api.php
@@ -170,6 +170,8 @@ Route::controller(DoctorController::class)->prefix('doctors')->group(function ()
     Route::get('{id}', 'show');
     Route::put('{id}', 'update')->middleware(['auth:api', 'doctor']);
     Route::post('{doctorId}/addServices', 'addService');
+    Route::post('{id}/certificates', 'addCertificates');
+
     Route::delete('{id}', 'destroy')->middleware(['auth:api', 'center']);
     Route::put('block/{id}', 'block');//->middleware(['auth:api', 'doctor']);
     Route::put('unBlock/{id}', 'unBlock');
